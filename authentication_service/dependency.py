05f7e21e4ff5249771bab97bfc56ea59db4821f9
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-19 20:11:01 +0800

    Delete authentication_service directory

--- authentication_service/dependency.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
-
-
-# Initial database for ticket booking service
-def init_database():
-    db.drop_all()
-    db.create_all()
--- authentication_service/manage.py
+++
@@ -1,7 +0,0 @@
-from authentication_service.main import app
-from authentication_service.models import db, Auth
-
-
-@app.shell_context_processor
-def make_shell_context():
-    return dict(app=app, db=db, Auth=Auth)
